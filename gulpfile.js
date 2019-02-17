0bc3704d7f233d60cada0ef6f65851a0a5ba3cff
JD <jan-dennisd@outlook.at>
2019-02-17 21:52:32 +0100

    include gulp and all plugins

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// gulp.js include
+var gulp = require('gulp');
+
+// include pluginns
+var changed = require('gulp-changed'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp- uglify'),
+    rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    minifyhtml = require ('gulp-minify-html'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css');
+
+    
