0537ffb23ba6711934dcd147e5e4cb1a29dc6cdc
JD <jan-dennisd@outlook.at>
2019-04-07 21:58:15 +0200

    furst try

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 // variable section
-const { dest, series, src, watch } = require('gulp')
-const del = require("del")
-const sass = require('gulp-sass')
+const {
+  dest,
+  series,
+  src,
+  watch
+} = require('gulp')
 const autoprefixer = require('gulp-autoprefixer')
+const browsersync = require("browser-sync").create()
+const concat = require('gulp-concat')
+const del = require("del")
+const nunjucksRender = require("gulp-nunjucks-render")
+const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 
 const dist = "dist/"
 const source = "src/"
 
 const css = {
-  in: source + "sass/*.scss",
+  in: source + "sass/app.scss",
   out: dist + "css/",
   sassOpts: {
     outputStyle: "compressed",
@@ -21,13 +29,39 @@ const css = {
   watch: source + "sass/**/*"
 }
 
-// task section
+const js = {
+  in: source + "scripts/**/*.js",
+  out: dist + "js/",
+  watch: source + "scripts/**/*"
+};
 
+const nunjuck = {
+  in: source + "pages/**/*.html",
+  out: dist,
+  path: source + "templates",
+  watch: ""
+};
+
+const syncOpts = {
+  server: {
+    baseDir: dist,
+    index: "index.html"
+  },
+  open: true,
+  notify: true
+}
+
+// task section
+// ------------------------------
+// clean task
 function clean(cb) {
   del([dist + "*"])
   cb()
 }
+// ------------------------------
 
+// ------------------------------
+// style task
 function style(cb) {
   src(css.in)
     .pipe(sourcemaps.init())
@@ -35,8 +69,45 @@ function style(cb) {
     .pipe(autoprefixer(css.autoprefixerOpts))
     .pipe(sourcemaps.write('.'))
     .pipe(dest(css.out))
-  watch(css.watch, style)
+  watch(css.watch, series(style, browsersync.reload))
   cb()
 }
+// ------------------------------
 
-exports.default = series(clean, style)
+// ------------------------------
+// script task
+function script(cb) {
+  src(js.in)
+    .pipe(sourcemaps.init())
+    .pipe(concat("app.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest(js.out))
+  watch(js.watch, series(script, browsersync.reload))
+  cb()
+}
+// ------------------------------
+
+
+// ------------------------------
+// html task
+function html(cb) {
+  src(nunjuck.in)
+    .pipe(nunjucksRender({
+      path: nunjuck.path
+    })
+    )
+    .pipe(dest(nunjuck.out))
+  watch(nunjuck.watch, series(html, browsersync.reload))
+  cb()
+}
+// ------------------------------
+
+// ------------------------------
+// browser sync task
+function bSync(cb) {
+  browsersync.init(syncOpts)
+  cb()
+}
+// ------------------------------
+
+exports.default = series(clean, style, script, html, bSync)
